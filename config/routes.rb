2c1574a097b8d1a2df4f25b0fd2db6421efde342
kouta055 <hayabusa020255@gmail.com>
2021-05-04 23:57:35 +0900

    削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get '/main' => 'profile#main'
   get '/articles' => 'articles#new'
 
-  resources :articles, only: [:show, :new, :create, :edit, :update]
+  resources :articles, only: [:show, :new, :create, :edit, :update, :destroy]
 end
