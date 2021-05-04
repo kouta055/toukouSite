7b1cdf1ceec0680e023b698fefa18c0a1da5f1f3
kouta055 <hayabusa020255@gmail.com>
2021-05-05 00:11:19 +0900

    rails db:migrate:reset実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_093349) do
+ActiveRecord::Schema.define(version: 2021_04_22_102718) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
