3db9d020a40bfeb91d6b0db1bd4693f481baae02
kouta055 <hayabusa020255@gmail.com>
2021-06-06 06:58:33 +0900

    User修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do
-  Article.create(
+no1 = User.create!(email: 'sample1@email.com', password: '1234567')
+no2 = User.create!(email: 'sample2@email.com', password: '1234568')
+
+5.times do
+  no1.articles.create!(
+    title: Faker::Lorem.sentence(word_count: 5),
+    content: Faker::Lorem.sentence(word_count: 100)
+  )
+end
+
+5.times do
+  no2.articles.create!(
     title: Faker::Lorem.sentence(word_count: 5),
     content: Faker::Lorem.sentence(word_count: 100)
   )
