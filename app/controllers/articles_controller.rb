0a9b209de3982474d38d3f683c7b9e257b4ff78b
kouta055 <hayabusa020255@gmail.com>
2021-06-10 07:02:21 +0900

    show comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,19 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
+
   def show
+    @comments = @article.comments
   end
+
   def new
     @article = current_user.articles.build
   end
+
   def create
     @article = current_user.articles.build(article_params)
     if @article.save
@@ -18,9 +23,11 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
+
   def edit
     @article = current_user.articles.find(params[:id])
   end
+
   def update
     @article = current_user.articles.find(params[:id])
     if @article.update(article_params)
@@ -36,10 +43,12 @@ class ArticlesController < ApplicationController
     article.destroy!
     redirect_to root_path, notice: '削除に成功しました'
   end
+
   private
   def article_params
     params.require(:article).permit(:title, :content)
   end
+
   def set_article
     @article = Article.find(params[:id])
   end
