ed38d65f964e974b2d49bd344c6f228ac5e02212
kouta055 <hayabusa020255@gmail.com>
2021-06-19 15:12:21 +0900

    いいね機能の追加　削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy
-
+  has_many :likes, dependent: :destroy
   has_one :profile, dependent: :destroy
 
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
@@ -33,6 +33,10 @@ class User < ApplicationRecord
 
   def has_written?(article)
     articles.exists?(id: article.id)
+  end
+
+  def has_liked?(article)
+    likes.exists?(article_id: article.id)
   end
 
   def display_name
