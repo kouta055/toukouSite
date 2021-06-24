5a07b4eaa7b215e1a6b1b3af5baa8980e2c555eb
kouta055 <hayabusa020255@gmail.com>
2021-06-24 17:39:48 +0900

    ActionTextの追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -20,9 +20,7 @@ class Article < ApplicationRecord
   validates :title, length: { minimum: 2, maximum: 100 }
   validates :title, format: { with: /\A(?!\@)/ }
   validates :content, presence: true
-  validates :content, length: { minimum: 10 }
-  validates :content, uniqueness: true
-  validate :validate_title_and_content_length
+
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   belongs_to :user
@@ -37,11 +35,5 @@ class Article < ApplicationRecord
 
   def like_count
     likes.count
-  end
-
-  private
-  def validate_title_and_content_length
-    char_count = self.title.length + self.content.length
-    errors.add(:content, '10文字以上で') unless char_count > 10
   end
 end
